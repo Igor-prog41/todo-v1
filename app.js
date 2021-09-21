79df77f4f8d9f311ad89ad15ee2c800cbf81d12a
igor <iaroshevych.i@gmail.com>
2021-09-20 20:59:48 -0600

    add connection to SCC

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 
 const app= express();
 
-app.use(express.urlencoded({extended:true}));
-
 app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 const options = {
     weekday: 'long',
