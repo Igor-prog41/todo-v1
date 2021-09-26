9e3095c65159d520cb0406f2cef07087c1d1d972
igor <iaroshevych.i@gmail.com>
2021-09-25 21:54:14 -0600

    add base layouts

--- app.js
+++ app.js
@@ -1,40 +1,58 @@
-const express = require('express');
+const express = require("express");
 
 const app= express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 const options = {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long'
+    weekday: "long",
+    day: "numeric",
+    month: "long"
 };
 
-let itemsForTodoList=['Buy food','Cook food','Eat food'];
+let itemsForTodoList=["Buy food","Cook food","Eat food"];
+let itemsForWorkTodoList=["Buy pen","Write letter","Send letter"];
 
-app.get('/',(req,res)=>{
+app.get("/",(req,res)=>{
 
     const today =new Date();
 
-    const  dayToday = today.toLocaleDateString('en-US',options);
+    const  dayToday = today.toLocaleDateString("en-US",options);
    
-    res.render('list', { kindOfDay: dayToday,
+    res.render("list", { listTitle: dayToday,
                             todoListForHtml: itemsForTodoList});
     // <!-- <%= kindOfDay %> -->
 
 });
 
-app.post('/',(req,res)=>{
-    // console.log(req.body.newItem);
+app.get("/work",(req,res)=>{
+    res.render("list", { listTitle:"Work List",
+                         todoListForHtml : itemsForWorkTodoList })
+})
 
-    itemsForTodoList.push(req.body.newItem);
- 
-    res.redirect('/');
+app.get("/about",(req,res)=>{
+    res.render("about")
+})
+// listTitle:"Work List"  newListItems
+
+
+app.post('/',(req,res)=>{
+    //  console.log(req.body);
+
+    if (req.body.list==="Work"){
+        itemsForWorkTodoList.push(req.body.newItem);
+        res.redirect("/work");
+    }
+    else{
+        itemsForTodoList.push(req.body.newItem);
+        res.redirect("/");
+    }
+
 })
 
 app.listen(3000, function(){
-    console.log('sever started on port 3000');
+    console.log("sever started on port 3000");
 })
